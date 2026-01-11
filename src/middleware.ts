528b853a4c885cba0e3af6415d2230e3c579934d
zundo <zundo@example.com>
2026-01-12 05:16:19 +0900

    Exclude API from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,8 +12,8 @@ export const config = {
          * - _next/static (static files)
          * - _next/image (image optimization files)
          * - favicon.ico (favicon file)
-         * Feel free to modify this pattern to include more paths.
+         * - /api (API routes) <-- 【重要】ここを追加してAPIを除外しました
          */
-        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        '/((?!_next/static|_next/image|favicon.ico|api|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 }
